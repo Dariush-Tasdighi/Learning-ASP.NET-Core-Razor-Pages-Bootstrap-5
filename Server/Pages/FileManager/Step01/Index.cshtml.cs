e4599f234f90a6ac4ddb8f31eb5689dbcb969bd5
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2022-04-10 07:14:35 +0430

    Making some changes!

---
+++ Server/Pages/FileManager/Step01/Index.cshtml.cs
@@ -0,0 +1,145 @@
+﻿using System.Linq;
+
+namespace Server.Pages.FileManager.Step01
+{
+	public class IndexModel : Infrastructure.BasePageModel
+	{
+		#region Constructor
+		public IndexModel
+			(Microsoft.Extensions.Hosting.IHostEnvironment hostEnvironment) : base()
+		{
+			HostEnvironment = hostEnvironment;
+
+			DisplayDateTimeFormat =
+				"yyyy/MM/dd [HH:mm:ss]";
+
+			PageAddress =
+				"/Admin/FileManager/Index";
+
+			PhysicalRootPath =
+				$"{HostEnvironment.ContentRootPath}wwwroot"
+				.Replace("/", "\\");
+
+			// برای خلاص شدن از شر اخطار نال بودن
+			Files = new System.Collections.Generic.List<System.IO.FileInfo>();
+			Directories = new System.Collections.Generic.List<System.IO.DirectoryInfo>();
+		}
+		#endregion /Constructor
+
+		#region Public Read Only Property(ies)
+		public string PageAddress { get; }
+
+		public string PhysicalRootPath { get; }
+
+		public string? DisplayDateTimeFormat { get; }
+
+		public Microsoft.Extensions.Hosting.IHostEnvironment HostEnvironment { get; }
+		#endregion /Public Read Only Property(ies)
+
+		#region Public Property(ies)
+		public string? CurrentPath { get; set; }
+
+		public string? PhysicalCurrentPath { get; set; }
+
+		public System.Collections.Generic.IList<System.IO.FileInfo> Files { get; set; }
+
+		public System.Collections.Generic.IList<System.IO.DirectoryInfo> Directories { get; set; }
+		#endregion /Public Property(ies)
+
+		#region OnGet
+		public void OnGet(string? path)
+		{
+			CheckPathAndSetCurrentPath(path: path);
+
+			SetDirectoriesAndFiles();
+		}
+		#endregion /OnGet
+
+		#region CheckPathAndSetCurrentPath
+		/// <summary>
+		/// قانون
+		///
+		/// CurrentPath:
+		///		/
+		///		/Images/
+		///
+		/// یعنی همیشه دو طرف آن / دارد
+		/// </summary>
+		public void CheckPathAndSetCurrentPath(string? path)
+		{
+			// **************************************************
+			string fixedPath = "/";
+
+			if (string.IsNullOrWhiteSpace(path) == false)
+			{
+				fixedPath =
+					path.Replace("\\", "/");
+
+				if (fixedPath.StartsWith("/") == false)
+				{
+					fixedPath =
+						$"/{fixedPath}";
+				}
+
+				if (fixedPath.EndsWith("/") == false)
+				{
+					fixedPath =
+						$"{fixedPath}/";
+				}
+
+				while (fixedPath.Contains("//"))
+				{
+					fixedPath =
+						fixedPath.Replace("//", "/");
+				}
+			}
+			// **************************************************
+
+			// **************************************************
+			CurrentPath = fixedPath;
+			// **************************************************
+
+			// **************************************************
+			PhysicalCurrentPath =
+				$"{PhysicalRootPath}{CurrentPath}"
+				.Replace("/", "\\");
+
+			if (System.IO.Directory.Exists(path: PhysicalCurrentPath) == false)
+			{
+				PhysicalCurrentPath = PhysicalRootPath;
+			}
+			// **************************************************
+		}
+		#endregion /CheckPathAndSetCurrentPath
+
+		#region SetDirectoriesAndFiles
+		public void SetDirectoriesAndFiles()
+		{
+			if (string.IsNullOrWhiteSpace(PhysicalCurrentPath) ||
+				System.IO.Directory.Exists(PhysicalCurrentPath) == false)
+			{
+				Files = new System.Collections.Generic.List<System.IO.FileInfo>();
+				Directories = new System.Collections.Generic.List<System.IO.DirectoryInfo>();
+
+				return;
+			}
+
+			var directoryInfo =
+				new System.IO.DirectoryInfo(path: PhysicalCurrentPath);
+
+			Files =
+				directoryInfo.GetFiles()
+				.OrderBy(current => current.Extension)
+				.ThenBy(current => current.Name)
+				.ToList()
+				;
+
+			Directories =
+				directoryInfo.GetDirectories()
+				.OrderBy(current => current.Name)
+				.ToList()
+				;
+		}
+		#endregion /SetDirectoriesAndFiles
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,8 @@
 // **************************************************
+// **************************************************
+// **************************************************
+// Step 01
+// **************************************************
 //var builder = WebApplication.CreateBuilder(args);
 //var app = builder.Build();
 
@@ -6,26 +10,92 @@
 
 //app.Run();
 // **************************************************
+// **************************************************
+// **************************************************
 
+// **************************************************
+// **************************************************
+// **************************************************
+// Step 02
+// **************************************************
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.Extensions.DependencyInjection;
+
+//var builder =
+//	Microsoft.AspNetCore.Builder
+//	.WebApplication.CreateBuilder(args: args);
+
+//// AddRazorPages() -> using Microsoft.Extensions.DependencyInjection;
+//builder.Services.AddRazorPages();
+
+//var app =
+//	builder.Build();
+
+//// UseStaticFiles() -> using Microsoft.AspNetCore.Builder;
+//app.UseStaticFiles();
+
+//// MapRazorPages() -> using Microsoft.AspNetCore.Builder;
+//app.MapRazorPages();
+
+//app.Run();
+// **************************************************
+// **************************************************
+// **************************************************
+
+// **************************************************
+// **************************************************
+// **************************************************
+// Step 03
 // **************************************************
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
+var webApplicationOptions =
+	new Microsoft.AspNetCore.Builder.WebApplicationOptions
+	{
+		EnvironmentName =
+			System.Diagnostics.Debugger.IsAttached ?
+			Microsoft.Extensions.Hosting.Environments.Development
+			:
+			Microsoft.Extensions.Hosting.Environments.Production,
+	};
+
 var builder =
 	Microsoft.AspNetCore.Builder
-	.WebApplication.CreateBuilder(args: args);
+	.WebApplication.CreateBuilder(options: webApplicationOptions);
 
+// **************************************************
+// AddHttpContextAccessor() -> using Microsoft.Extensions.DependencyInjection;
+builder.Services.AddHttpContextAccessor();
+// **************************************************
+
+// **************************************************
 // AddRazorPages() -> using Microsoft.Extensions.DependencyInjection;
 builder.Services.AddRazorPages();
+// **************************************************
 
+// **************************************************
 var app =
 	builder.Build();
+// **************************************************
 
+// **************************************************
+// UseHttpsRedirection() -> using Microsoft.AspNetCore.Builder;
+app.UseHttpsRedirection();
+// **************************************************
+
+// **************************************************
 // UseStaticFiles() -> using Microsoft.AspNetCore.Builder;
 app.UseStaticFiles();
+// **************************************************
 
+// **************************************************
 // MapRazorPages() -> using Microsoft.AspNetCore.Builder;
 app.MapRazorPages();
+// **************************************************
 
+// **************************************************
 app.Run();
+// **************************************************
+// **************************************************
 // **************************************************
