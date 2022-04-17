aa5d234f8b32ee9bca8cb778fbdfe2fe86387e33
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2022-04-17 16:12:06 +0430

    Making some changes!

---
+++ Server/Pages/FileManager/Step03/CreateDirectory8.cshtml.cs
@@ -0,0 +1,66 @@
+namespace Server.Pages.FileManager.Step03
+{
+	public class CreateDirectoryModel8 : Infrastructure.BasePageModel
+	{
+		public CreateDirectoryModel8() : base()
+		{
+			ViewModel = new();
+		}
+
+		public void OnGet()
+		{
+		}
+
+		[Microsoft.AspNetCore.Mvc.BindProperty]
+		public ViewModels.FileManager.Step3.CreateDirectory8ViewModel ViewModel { get; set; }
+
+		public void OnPost()
+		{
+			if (ModelState.IsValid == false)
+			{
+				return;
+			}
+
+			// **************************************************
+			AddErrorToast(message: null);
+			AddErrorToast(message: string.Empty);
+			AddErrorToast(message: "               ");
+
+			AddErrorToast(message: "     Error     Toast     (1)     ");
+
+			AddErrorToast(message: "Error Toast (2)");
+			AddErrorToast(message: "Error Toast (3)");
+
+			AddErrorToast(message: "Error Toast (3)");
+			// **************************************************
+
+			// **************************************************
+			AddWarningToast(message: null);
+			AddWarningToast(message: string.Empty);
+			AddWarningToast(message: "               ");
+
+			AddWarningToast(message: "     Warning     Toast     (1)     ");
+
+			AddWarningToast(message: "Warning Toast (2)");
+			AddWarningToast(message: "Warning Toast (3)");
+
+			AddWarningToast(message: "Warning Toast (3)");
+			// **************************************************
+
+			// **************************************************
+			AddSuccessToast(message: null);
+			AddSuccessToast(message: string.Empty);
+			AddSuccessToast(message: "               ");
+
+			AddSuccessToast(message: "     Success     Toast     (1)     ");
+
+			AddSuccessToast(message: "Success Toast (2)");
+			AddSuccessToast(message: "Success Toast (3)");
+
+			AddSuccessToast(message: "Success Toast (3)");
+			// **************************************************
+
+			return;
+		}
+	}
+}
